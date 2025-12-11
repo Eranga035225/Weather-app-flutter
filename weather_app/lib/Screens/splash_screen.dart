8c33a432c8a84cf441c4ce95a5d52ce07c353c59
Eranga <erangakavishanka@icloud.com>
2025-12-11 15:04:59 +0530

    added a lottie

--- weather_app/lib/Screens/splash_screen.dart
+++ weather_app/lib/Screens/splash_screen.dart
@@ -11,6 +11,10 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text('Weather App') 
+      )
 
 
 
