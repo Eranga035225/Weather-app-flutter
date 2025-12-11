d235904afc450fd02875c517439c922f16049821
Eranga <erangakavishanka@icloud.com>
2025-12-11 17:23:01 +0530

    added section1

--- weather_app/lib/Screens/home_screen.dart
+++ weather_app/lib/Screens/home_screen.dart
@@ -12,6 +12,58 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      body: Column(children: [
+        SizedBox(
+          height: 380,
+          child: Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 350,
+                decoration: BoxDecoration(
+                  color : Colors.grey,
+                 
+                ),  
+              ),
+              SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Row(children: [
+                    CircleAvatar(
+                      child: Icon(Icons.menu, color: Colors.white,),
+                      backgroundColor: Colors.black45,
+                    ),
+                    Text('MeteoNow', style: TextStyle(color: Colors.white, fontSize: 22)),
+                  
+                  ],),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: Align(
+                  alignment: AlignmentGeometry.bottomCenter,
+                  child: Container(
+                    width: double.infinity,
+                    height: 60,
+                    decoration: BoxDecoration(
+                       color: Colors.grey.shade200,
+                       borderRadius: BorderRadius.circular(20)
+                    ),
+                   
+                
+                  )
+                ),
+              )
+            ],
+          ),
+        )
+
+
+      ],)
+
+
+
 
     );
   }
