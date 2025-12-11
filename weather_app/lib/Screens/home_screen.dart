0ebf42fd1f2692046db39088e07e0fb34032f00e
Eranga <erangakavishanka@icloud.com>
2025-12-11 17:31:58 +0530

    developed

--- weather_app/lib/Screens/home_screen.dart
+++ weather_app/lib/Screens/home_screen.dart
@@ -28,12 +28,20 @@ class _HomeScreenState extends State<HomeScreen> {
               SafeArea(
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                  child: Row(children: [
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
                     CircleAvatar(
                       child: Icon(Icons.menu, color: Colors.white,),
                       backgroundColor: Colors.black45,
                     ),
                     Text('MeteoNow', style: TextStyle(color: Colors.white, fontSize: 22)),
+                    CircleAvatar(
+                     
+                      backgroundColor: Colors.white,
+                      backgroundImage: NetworkImage('https://th.bing.com/th/id/R.6b0022312d41080436c52da571d5c697?rik=ejx13G9ZroRrcg&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fuser-png-icon-young-user-icon-2400.png&ehk=NNF6zZUBr0n5i%2fx0Bh3AMRDRDrzslPXB0ANabkkPyv0%3d&risl=&pid=ImgRaw&r=0'),
+                    )
+                    
                   
                   ],),
                 ),
