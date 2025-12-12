1c99dd6bc8ec2b18a8a360128ab855958be98a09
Eranga <erangakavishanka@icloud.com>
2025-12-12 17:04:34 +0530

    endpoint

--- weather_app/lib/services/weather_services.dart
+++ weather_app/lib/services/weather_services.dart
@@ -1,10 +1,12 @@
 class WeatherServices {
 
-  //http://api.weatherapi.com/v1/current.json?key=de4bf245cadd4ba787f60126251112&q=Colombo&aqi=no
+  //
 
 
-  Future<void> getCurrentWeather() async {
-    
+  Future<void> getCurrentWeather(String query) async {
+    final endpoint = 'http://api.weatherapi.com/v1/current.json?key=de4bf245cadd4ba787f60126251112&q=${query}';
+
+
 
 
 
