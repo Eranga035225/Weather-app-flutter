928690bc6cabcc97be7356f658fa16c53a0311b7
Eranga <erangakavishanka@icloud.com>
2025-12-12 17:25:32 +0530

    assigend to a response

--- weather_app/lib/services/weather_services.dart
+++ weather_app/lib/services/weather_services.dart
@@ -5,7 +5,7 @@ class WeatherServices {
   Future<void> getCurrentWeather(String query) async {
     final endpoint = 'http://api.weatherapi.com/v1/current.json?key=de4bf245cadd4ba787f60126251112&q=$query';
 
-    await http.get(Uri.parse(endpoint));
+    final response = await http.get(Uri.parse(endpoint));
 
 
 
