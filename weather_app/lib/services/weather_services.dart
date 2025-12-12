58561efec78dc39705f8c624968c9a3ae717e422
Eranga <erangakavishanka@icloud.com>
2025-12-12 16:56:14 +0530

    created functions

--- weather_app/lib/services/weather_services.dart
+++ weather_app/lib/services/weather_services.dart
@@ -1,6 +1,16 @@
 class WeatherServices {
 
+  //http://api.weatherapi.com/v1/current.json?key=de4bf245cadd4ba787f60126251112&q=Colombo&aqi=no
 
+
+  Future<void> getCurrentWeather() async {
+    
+
+
+
+
+
+  }
 
 
 
