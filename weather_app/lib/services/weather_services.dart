cd00bbabc866dcad6b1d2b1e274598e4ed383936
Eranga <erangakavishanka@icloud.com>
2026-01-12 11:11:50 +0530

    added commnets

--- weather_app/lib/services/weather_services.dart
+++ weather_app/lib/services/weather_services.dart
@@ -5,7 +5,7 @@ class WeatherServices {
   Future<void> getCurrentWeather(String query) async {
     final endpoint = 'http://api.weatherapi.com/v1/current.json?key=de4bf245cadd4ba787f60126251112&q=$query';
 
-    final response = await http.get(Uri.parse(endpoint));
+    final response = await http.get(Uri.parse(endpoint)); //string should be converted in to an URI before passing
 
     if(response.statusCode == 200){
       //success
